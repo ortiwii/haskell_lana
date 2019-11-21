65b27ed2d574ba761801a889787283b5bdbcd92c
jonr <jrenteria003@ikasle.ehu.eus>
2019-11-21 20:04:28 +0100

    aldaketak, lengoaiakoa_da

--- Funtzioak.hs
+++ Funtzioak.hs
@@ -2,13 +2,12 @@ import Motak
 
 af_da :: Af -> Bool
 af_da (a,b,c,d,e,f) 
-    |null(Egoerak)		= error "Egoerak ez daude definituta"
-    |null(Alfabetoa)	= error "Alfabetoa ez dago definituta"
-    |null(A_trantsizioak)	= error "A_trantsizioak ez daude definituta"
-    |null(E_trantsizioak)	= error "E_trantsizioak ez daude definituta" 
-    |null(HasierakoEgoera)	= error "HasierakoEgoera ez dago definituta"
-    |null(AzkenEgoerak)	= error "AzkenEgoerak ez daude definituta"
-    |otherwise		= not(errepikatzen_da(a)) && not(errepikatzen_da(b)) && badago e a && badaude a b c
+    |null(a)        = error "Egoerak ez daude definituta"
+    |null(b)        = error "Alfabetoa ez dago definituta"
+    |null(c)        = error "A_trantsizioak ez daude definituta"
+    |null(d)        = error "E_trantsizioak ez daude definituta" 
+    |null(f)        = error "AzkenEgoerak ez daude definituta"
+    |otherwise      = not(errepikatzen_da(a)) && not(errepikatzen_da(b)) && badago e a && badaude a b c
 
 errepikatzen_da :: Eq t => [t] -> Bool
 errepikatzen_da [] = False
@@ -35,11 +34,20 @@ badaude s z u
     | badago_egoera s (head u) && badago_sinbol z (head u)  = badaude s z (tail u)
     | otherwise     = False 
 
-lengoaiakoa_da :: Af -> Hitza -> Bool
-lengoaiakoa_da (a,b,c,d,e,f) hitza
+hitza_dago :: Af -> Hitza -> Bool
+hitza_dago(a,b,c,d,e,f) hitza
     | (null hitza) = True
     | badago_sinbol b (0,(head hitza),0)    = lengoaiakoa_da (a,b,c,d,e,f) (tail hitza)
     | otherwise    = False
+
+lengoaiakoa_da :: Af -> Hitza -> Bool
+lengoaiakoa_da (a,b,c,d,e,f) hitza
+    | not(af_da (a,b,c,d,e,f))  = False
+    | not(hitza_dago (a,b,c,d,e,f) hitza)   = False
+    | 
+    | otherwise =
+
+lengoaiakoa_da_lag :: Af -> Hitza -> 
 
 --sailkatu :: Af -> Af_motak
 --sailkatu af
